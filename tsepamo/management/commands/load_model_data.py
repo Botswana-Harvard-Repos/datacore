4c28cd3ee1f7c93ee081bdd19dc522ffd1dc163b
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-05-10 10:00:10 +0200

    Fix file names

--- tsepamo/management/commands/load_model_data.py
+++ tsepamo/management/commands/load_model_data.py
@@ -7,10 +7,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        csv_files=[('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo 1.csv',['tsepamo.tsepamoone','tsepamo.outcomesone'])
-                   ('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo 2.csv',['tsepamo.tsepamotwo','tsepamo.outcomestwo','tsepamo.switcheripmstwo','tsepamo.personalidentifierstwo','tsepamo.ipms'])
-                   ('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo 3.csv',['tsepamo.tsepamothree','tsepamo.outcomesthree','tsepamo.switcheripmsthree','personalidentifiersthree'])
-                   ('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo 4.csv',['tsepamo.tsepamofour','tsepamo.outcomesfour','tsepamo.switcheripmsfour','tsepamo.personalidentifiersfour'])] 
+        csv_files=[('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo_1.csv',['tsepamo.tsepamoone','tsepamo.outcomesone'])
+                   ('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo_2.csv',['tsepamo.tsepamotwo','tsepamo.outcomestwo','tsepamo.switcheripmstwo','tsepamo.personalidentifierstwo','tsepamo.ipms'])
+                   ('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo_3.csv',['tsepamo.tsepamothree','tsepamo.outcomesthree','tsepamo.switcheripmsthree','personalidentifiersthree'])
+                   ('/Users/kebadiretsemotlhabi/source/datacore-project/datacore/Tsepamo_4.csv',['tsepamo.tsepamofour','tsepamo.outcomesfour','tsepamo.switcheripmsfour','tsepamo.personalidentifiersfour'])] 
         tsepamo_data = LoadCSVData()
         tsepamo_data.load_model_data_all(csv_files)
 
