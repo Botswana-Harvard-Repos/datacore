75574f76109a6548caef34d82ade144d71a5c08e
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-05-13 10:25:27 +0200

    Added the task and error handling

--- tsepamo/tasks.py
+++ tsepamo/tasks.py
@@ -1,5 +1,13 @@
 from celery import shared_task
+from django.core.management import call_command
+import logging
+logger = logging.getLogger(__name__)
 
-@shared_task()
-def load_model_data_task(self):
-    pass
+@shared_task(bind=True)
+def run_load_model_data_task(self):
+    try:
+        call_command('load_model_data')
+    except Exception as exc:
+        logger.exception("An error occured during loading data")
+        raise exc
+    
