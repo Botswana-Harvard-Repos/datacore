5dc4d9a49bfbd8460f4c50bffd4918c830dee96f
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-05-15 10:17:47 +0200

    edit file path

--- tsepamo/tasks.py
+++ tsepamo/tasks.py
@@ -6,7 +6,7 @@ from tsepamo.utils import LoadCSVData
 
 @shared_task(bind=True)
 def run_load_model_data_task(self):
-    csv_files=[('/home/datacore/source/datacore/Tsepamo 1.csv',['tsepamo.tsepamoone','tsepamo.outcomesone']),
+    csv_files=[('/home/datacore/source/datacore/Tsepamo_1.csv',['tsepamo.tsepamoone','tsepamo.outcomesone']),
                    ('/home/datacore/source/datacore/Tsepamo_2.csv',['tsepamo.tsepamotwo','tsepamo.outcomestwo','tsepamo.switcheripmstwo','tsepamo.personalidentifierstwo','tsepamo.ipms']),
                    ('/home/datacore/source/datacore/Tsepamo_3.csv',['tsepamo.tsepamothree','tsepamo.outcomesthree','tsepamo.switcheripmsthree','personalidentifiersthree']),
                    ('/home/datacore/source/datacore/Tsepamo_4.csv',['tsepamo.tsepamofour','tsepamo.outcomesfour','tsepamo.switcheripmsfour','tsepamo.personalidentifiersfour'])] 
