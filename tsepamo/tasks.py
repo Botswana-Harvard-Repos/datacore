9eb506e05c836bd7fcd3fa2b33b5832296da2512
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-05-27 10:32:17 +0200

    remove loaded data

--- tsepamo/tasks.py
+++ tsepamo/tasks.py
@@ -6,8 +6,12 @@ logger = logging.getLogger('celery_progress')
 @shared_task()
 def run_load_model_data_task():
     logger.debug("The data files")
-    csv_files=[('/home/datacore/source/datacore/Tsepamo_1.csv',['tsepamo.tsepamoone','tsepamo.outcomesone']),
-                   ('/home/datacore/source/datacore/Tsepamo_2.csv',['tsepamo.tsepamotwo','tsepamo.outcomestwo','tsepamo.switcheripmstwo','tsepamo.personalidentifierstwo','tsepamo.ipmstwo']),
+    done_csv_files=[
+        #('/home/datacore/source/datacore/Tsepamo_1.csv',['tsepamo.tsepamoone','tsepamo.outcomesone']),
+                   #('/home/datacore/source/datacore/Tsepamo_2.csv',['tsepamo.tsepamotwo','tsepamo.outcomestwo','tsepamo.switcheripmstwo','tsepamo.personalidentifierstwo']),
+        ] 
+    csv_files=[
+                   ('/home/datacore/source/datacore/Tsepamo_2.csv',['tsepamo.ipmstwo']),
                    ('/home/datacore/source/datacore/Tsepamo_3.csv',['tsepamo.tsepamothree','tsepamo.outcomesthree','tsepamo.switcheripmsthree','personalidentifiersthree']),
                    ('/home/datacore/source/datacore/Tsepamo_4.csv',['tsepamo.tsepamofour','tsepamo.outcomesfour','tsepamo.switcheripmsfour','tsepamo.personalidentifiersfour'])] 
     try:
