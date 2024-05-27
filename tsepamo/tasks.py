9e8b601d26cc2b4b58d3a160bed3efbc3ffdc1a2
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-05-27 10:16:30 +0200

    correct model name

--- tsepamo/tasks.py
+++ tsepamo/tasks.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('celery_progress')
 def run_load_model_data_task():
     logger.debug("The data files")
     csv_files=[('/home/datacore/source/datacore/Tsepamo_1.csv',['tsepamo.tsepamoone','tsepamo.outcomesone']),
-                   ('/home/datacore/source/datacore/Tsepamo_2.csv',['tsepamo.tsepamotwo','tsepamo.outcomestwo','tsepamo.switcheripmstwo','tsepamo.personalidentifierstwo','tsepamo.ipms']),
+                   ('/home/datacore/source/datacore/Tsepamo_2.csv',['tsepamo.tsepamotwo','tsepamo.outcomestwo','tsepamo.switcheripmstwo','tsepamo.personalidentifierstwo','tsepamo.ipmstwo']),
                    ('/home/datacore/source/datacore/Tsepamo_3.csv',['tsepamo.tsepamothree','tsepamo.outcomesthree','tsepamo.switcheripmsthree','personalidentifiersthree']),
                    ('/home/datacore/source/datacore/Tsepamo_4.csv',['tsepamo.tsepamofour','tsepamo.outcomesfour','tsepamo.switcheripmsfour','tsepamo.personalidentifiersfour'])] 
     try:
